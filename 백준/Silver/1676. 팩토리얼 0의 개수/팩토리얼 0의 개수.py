aa30d949074f5e41b81b1f39681bf78bbb1e48ba
yujin5047 <167182662+yujin5047@users.noreply.github.com>
2026-01-08 22:10:36 +0900

    [Silver V] Title: 팩토리얼 0의 개수, Time: 36 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Silver/1676. 팩토리얼 0의 개수/팩토리얼 0의 개수.py
+++ 백준/Silver/1676. 팩토리얼 0의 개수/팩토리얼 0의 개수.py
@@ -1,19 +1,10 @@
-
 import sys
 input = sys.stdin.readline
 
 N = int(input())
 number = 1
-flag = False
-cnt = 0
 for i in range(N, 1, -1):
     number *= i
 str_num = str(number)
-for i in range(len(str_num) -1, 0, -1):
-    if "0" in str_num[i]:
-        cnt += 1
-        if str_num[i-1] != "0":
-            break
-    
-    
-print(cnt)
+
+print(len(str_num) - len(str_num.rstrip('0')))
