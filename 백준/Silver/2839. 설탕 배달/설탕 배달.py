ba6cff54786d62ac6e7c959cdaae092fa25932e2
yujin5047 <167182662+yujin5047@users.noreply.github.com>
2026-03-09 20:05:59 +0900

    [Silver IV] Title: 설탕 배달, Time: 40 ms, Memory: 32412 KB -BaekjoonHub

---
+++ 백준/Silver/2839. 설탕 배달/설탕 배달.py
@@ -0,0 +1,16 @@
+import sys
+input = sys.stdin.readline
+
+n = int(input())
+cnt = 0
+while n >= 0:
+    if n % 5 == 0:
+        cnt += n // 5
+        print(cnt)
+        break
+    else:
+        n -= 3
+        cnt += 1
+        continue
+if n < 0:
+    print(-1)
